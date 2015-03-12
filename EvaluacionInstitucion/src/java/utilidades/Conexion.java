01b178d6a0c0c1a7373fd791c08868d347178b6f
sebastiancar2 <felipe-12362@hotmail.com>
2015-03-12 11:05:23 -0500

    Conectar

--- EvaluacionInstitucion/src/java/utilidades/Conexion.java
+++ EvaluacionInstitucion/src/java/utilidades/Conexion.java
@@ -3,12 +3,45 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package utilidades;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
- * @author Sena
+ * @author User
  */
 public class Conexion {
-    
+     private static Connection conexion = null;
+    // variable estatica local.
+
+    private static void conectar() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            //  trae la libreria para poder realizar la conexion.
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/evaluacion", "root", "");
+            // la conexion tomara la direccion donde se encuentra la base de datos, la base de datos
+            // el usuario que usa la base de datos y el password de la misma
+            // en pocas palabras toma la direccion de la base de datos.
+        } catch (SQLException e) {
+            System.out.println("Error de MySQL: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error inesperado: " + e.getMessage());
+        } 
+    }
+
+    private Conexion() {
+        
+    }
+// Patron singleton
+    public static Connection getInstance() {
+        if (conexion == null) {
+            conectar();
+        }
+        return conexion;
+    }
 }
